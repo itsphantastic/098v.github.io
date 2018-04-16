85365caf31c119c22d171b9e4a801a18f86774f9
itsPHANtastic <Alison@Alisons-MacBook-Pro-2.local>
2018-04-16 11:29:31 -0400

    Fit Nutrition
    GBC Thesis 2018

--- js/template.js
+++ js/template.js
@@ -1,7 +1,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/Artboard 12.png');
 		
 		// Fixed header
 		//-----------------------------------------------
